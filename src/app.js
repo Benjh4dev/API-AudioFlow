73956f6b9c54cc770e157886f7d4c1421c7b2b3a
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-02 13:24:54 -0300

    inyectado el enrutador a la app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import "dotenv/config"
+import {router} from "../src/routes/index.js"
 
 const app = express()
 
 app.use(cors()) 
 app.use(express.json())
+app.use('/api', router)
 
 const port = process.env.PORT || 3001
 
