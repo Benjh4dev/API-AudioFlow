9448bb578ca88b1b42ebfdb19ae331121049e28b
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-03 20:38:49 -0300

    agregada función de hash de contraseña

---
+++ src/utils/passwordHash.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+const hashPassword = async (password) => {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  };
+
+export {hashPassword}
