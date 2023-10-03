a23a52d67cc287d00e60555db39c66a23e4084cd
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-03 20:38:33 -0300

    cambiado a firebase admin

--- src/services/user.js
+++ src/services/user.js
@@ -1,43 +1,38 @@
-import { collection, addDoc, query, getDocs, where } from "firebase/firestore";
-import bcrypt from "bcrypt";
 import db from "../firebase/config.js"
+import { hashPassword } from "../utils/passwordHash.js";
 
-const insertUser = async ({username, email, password}) => {
-    try {
-        const userCollection = collection(db, "user")
-        const saltRounds = 10
-        const hashedPassword = await bcrypt.hash(password, saltRounds)
+const insertUser = async ({ username, email, password }) => {
+  try {
+    const userCollection = db.collection("user")
+    const hashedPassword = await hashPassword(password)
 
-        const newUser = {
-          username,
-          email,
-          password: hashedPassword,
-        }
-    
-        const docRef = await addDoc(userCollection, newUser)
-        const user = { id: docRef.id, ...newUser }
-    
-        console.log("Usuario agregado con ID: ", docRef.id)
-    
-        return user
-      } catch (error) {
-        console.error("Error al agregar el usuario a la base de datos:", error)
-        return error; 
-      }
-}
+    const newUser = {
+      username,
+      email,
+      password: hashedPassword,
+    };
+
+    const docRef = await userCollection.add(newUser);
+    const user = { id: docRef.id, ...newUser }
+   
+    console.log("Usuario agregado con ID: ", docRef.id)
+    return user;
+  } catch (error) {
+    console.error("Error al agregar el usuario a la base de datos:", error)
+    throw error
+  }
+};
 
 const isEmailRegistered = async (email) => {
-    try {
-        const userCollection = collection(db, "user");
-    
-        const q = query(userCollection, where("email", "==", email));
-        const querySnapshot = await getDocs(q);
-    
-        return !querySnapshot.empty;
-      } catch (error) {
-        console.error("Error al verificar el correo electrónico en la base de datos:", error);
-        throw error;
-      }
-}
+  try {
+    const userCollection = db.collection("user")
+    const querySnapshot = await userCollection.where("email", "==", email).get()
+
+    return !querySnapshot.empty
+  } catch (error) {
+    console.error("Error al verificar el correo electrónico en la base de datos:", error)
+    throw error
+  }
+};
 
 export { insertUser, isEmailRegistered }
