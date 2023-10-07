870c89de70a9acfed253f99021f7396db54f983c
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-07 17:23:40 -0300

    agregado servicio para el login

---
+++ src/services/auth.js
@@ -0,0 +1,34 @@
+import db from "../firebase/config.js"
+import { comparePassword } from "../utils/passwordHandle.js";
+import { generateToken } from "../utils/jwtHandle.js";
+
+const authenticateUser = async (email, password) => {
+    try {
+        const userCollection = db.collection("user")
+        const snapshot = await userCollection.where("email", "==", email).get()
+
+        //NO EXISTE EL USUARIO EN BASE AL CORREO
+        if (snapshot.empty) {
+            return { found: false }
+        }
+
+        const userDoc = snapshot.docs[0]
+        const id = userDoc.id
+        const userData = userDoc.data()
+
+        const passwordMatch = await comparePassword(password, userData.password)
+        if (passwordMatch) {
+            delete userData.password
+            const token = generateToken(id)
+            return { found: true, token: token, user: userData }
+        } else {
+            return { found: false }
+        }
+
+    } catch (error) {
+        console.error("Error al autenticar el usuario:", error);
+        throw error;
+    }
+};
+
+export { authenticateUser }
