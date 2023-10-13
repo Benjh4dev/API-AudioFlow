68db598d403c261debc249412435f6ae401f4cde
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-13 16:38:16 -0300

    se inyectan los middlewares a la ruta

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { addUser, updateUser, getUser, deleteUser, getUsers } from "../controllers/user.js";
+import { addUser, updateUser, getUser, deleteUser, getUsers } from "../controllers/user.js"
+import { checkJwt } from "../middlewares/session.js"
+import { verifyUserId } from "../middlewares/verifyUserId.js"
 const router = Router()
 
 router.get('/:id', getUser)
-router.get('/', getUsers)
+router.get('/', checkJwt, getUsers)
 router.post('/', addUser)
-router.put('/:id', updateUser)
+router.patch('/:id', checkJwt, verifyUserId, updateUser)
 router.delete('/:id', deleteUser)
 
 export { router }
