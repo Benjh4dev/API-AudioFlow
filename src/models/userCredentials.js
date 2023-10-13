2d8b2d420607bbc61121bdbe057dfe888f27f951
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-13 14:49:30 -0300

    cambios en mensajes de error

--- src/models/userCredentials.js
+++ src/models/userCredentials.js
@@ -2,23 +2,21 @@ import { z } from 'zod';
 
 const userCredentialsModel = z.object({
     email: z.string({
-        message: 'El correo electrónico es requerido'
+        required_error: "El correo es requerido",
+        invalid_type_error: "El correo debe ser un texto",
     }).email({
         message: 'El correo electrónico no es válido'
     }).max(254, {
         message: 'El correo electrónico debe tener menos de 254 caracteres'
     }),
-    password: z.string().min(6, {
-        message: 'La contraseña debe tener al menos 6 caracteres'
-    }).max(64, {
-        message: 'La contraseña debe tener menos de 64 caracteres'
+    password: z.string({
+        required_error: "La contraseña es requerida",
+        invalid_type_error: "La contraseña debe ser un texto"
     })
 })
-
 
 const validateUserCredentials = (object) => {
     return userCredentialsModel.safeParse(object)
 }
-
 
 export { userCredentialsModel, validateUserCredentials }
