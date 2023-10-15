9068540a510101278104574c44871d4fb8ea51c7
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-14 23:12:11 -0300

    se agrega el bucket del storage de firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,15 @@
 import { config } from 'dotenv';
 
-import { initializeApp, applicationDefault } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp, applicationDefault } from "firebase-admin/app"
+import { getFirestore } from "firebase-admin/firestore"
+import { getStorage } from "firebase-admin/storage"
  
 // CONFIGURACION DE FIREBASE PARA LA BASE DE DATOS USANDO MODO ADMIN
 initializeApp({
   credential: applicationDefault(),
-  
+  storageBucket: "gs://audioflow-8f509.appspot.com"
 });
 
 const db = getFirestore();
-export default db;
+const storage = getStorage()
+export { db , storage }
