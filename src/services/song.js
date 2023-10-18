e7d5a8d9f2d8669acae9e3dc8d9397ca1ac04b85
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-18 14:44:25 -0300

    se agrega servicio de canción

---
+++ src/services/song.js
@@ -0,0 +1,24 @@
+import { db } from "../firebase/config.js"
+import { uploadToStorage } from "../utils/storageHandle.js"
+
+const insertSong = async ({ name, audio_file, cover_art }) => {
+    console.log(audio_file , cover_art)
+    const [coverURL, audioURL] = await Promise.all([
+        uploadToStorage(cover_art),
+        uploadToStorage(audio_file)
+    ])
+
+    const song = {
+        name,
+        coverURL,
+        audioURL,
+    }
+
+    const docRef = await db.collection('song').add(song)
+    const songWithID = { id: docRef.id, ...song }
+
+    console.log("Canción agregada con ID: ", docRef.id)
+    return songWithID
+}
+
+export { insertSong }
