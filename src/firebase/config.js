e7a47ab7524192a4a45a78f4bf5209e708a52178
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-18 17:54:53 -0300

    se agrega conexión al storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,17 @@
 import { config } from 'dotenv';
 
-import { initializeApp, applicationDefault } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp } from "firebase-admin/app"
+import { getFirestore } from "firebase-admin/firestore"
+import admin from 'firebase-admin';
+import { getStorage } from "firebase-admin/storage"
  
+const storageURL = process.env.STORAGE_URL
 // CONFIGURACION DE FIREBASE PARA LA BASE DE DATOS USANDO MODO ADMIN
 initializeApp({
-  credential: applicationDefault(),
-  
+  credential: admin.credential.applicationDefault(),
+  storageBucket: storageURL
 });
 
 const db = getFirestore();
-export default db;
+const storage = getStorage()
+export { db , storage }
