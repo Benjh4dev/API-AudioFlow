19dbc59fa7f5978654e860bc18e70b69043e8558
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-10-23 19:44:14 -0300

    se agrega validación

--- src/models/song.js
+++ src/models/song.js
@@ -4,13 +4,18 @@ const songModel = z.object({
     name: z.string({
         required_error: "El nombre de la canción es requerido",
         invalid_type_error: "El nombre de la canción debe ser un texto",
+    }).min(1, {
+        message: "La canción debe tener al menos 1 caracter"
     }).max(45, {
         message: 'La canción debe tener un máximo de 45 caracteres'
     }),
     artist: z.string({
         required_error: "El nombre del artista es requerido",
         invalid_type_error: "El nombre del artista debe ser un texto",
-    }).max(45, {
+    }).min(1, {
+        message: "La canción debe tener al menos 1 caracter"
+    })
+    .max(45, {
         message: 'El nombre del artista debe tener un máximo de 45 caracteres'
     }),
 })
