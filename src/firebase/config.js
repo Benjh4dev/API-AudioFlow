3a8b2fe0e2e1e0cda3b7c5c7e7273041d59e0c9d
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-11-09 20:16:15 -0300

    se agrega config para crear imagen docker

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,21 @@
 import { config } from 'dotenv';
-
 import { initializeApp } from "firebase-admin/app"
 import { getFirestore } from "firebase-admin/firestore"
 import admin from 'firebase-admin';
 import { getStorage } from "firebase-admin/storage"
- 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const credentialsPath = join(__dirname, 'credentials.json');
+
+console.log(credentialsPath)
 const storageURL = process.env.STORAGE_URL
 // CONFIGURACION DE FIREBASE PARA LA BASE DE DATOS USANDO MODO ADMIN
 initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(credentialsPath),
   storageBucket: storageURL
 });
 
