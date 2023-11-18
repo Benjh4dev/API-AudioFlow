4bc1960d33d1e27969bf9560ab26ca2652612b0c
Kishin520 <nicokishins@hotmail.com>
2023-11-18 15:45:43 -0300

    completado la route the playlist

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { checkJwt } from "../middlewares/session.js"
 import { verifyUserId } from "../middlewares/verifyUserId.js"
-import { uploadMiddleware } from "../middlewares/verifyUploadFile.js";
+import { addPlaylist, getPlaylist, getUserPlaylist, deletePlaylist } from "../controllers/playlist.js";
+import { checkTypes } from "../middlewares/checkType.js";
 
 const router = Router()
+
+router.post('/:id', checkJwt, verifyUserId, checkTypes , addPlaylist)
+router.get('/:id', checkJwt, getUserPlaylist)
+router.get('/', getPlaylist)
+router.delete('/:id', checkJwt, deletePlaylist)
 
 export { router }
