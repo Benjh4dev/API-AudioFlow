8e0c921fbd55f4cea511d9f8389f797e40d560ec
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-11-22 01:34:48 -0300

    feat: added personalized CORS middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import "dotenv/config"
 import {router} from "../src/routes/index.js"
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 
-app.use(cors()) 
+app.use(corsMiddleware()) 
 app.use(express.json())
+app.disable('x-powered-by')
 app.use('/api', router)
 
 const port = process.env.PORT || 3001
