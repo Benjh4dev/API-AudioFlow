1d84a68c3e4f09c9465cc6555a18ce2d81421f92
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-11-27 19:01:34 -0300

    fix: added id validation to request

--- src/middlewares/verifyPlaylistId.js
+++ src/middlewares/verifyPlaylistId.js
@@ -2,12 +2,20 @@ import { getPlaylistById } from "../services/playlist.js"
 const verifyPlaylistId = async (req, res, next) => {
     try {
         const playlist_id = req.params.playlistId
-        const response = await getPlaylistById(playlist_id)
+        const userId = req.user.id
+        console.log(userId)
+        const response = await getPlaylistById(playlist_id, userId)
         if (!response.found) {
             res.status(404)
             res.send({ message: `La playlist de ID: ${playlist_id} no se encontró` })
             return
         }
+        if (!response.valid) {
+            res.status(403)
+            res.send({ message: `La playlist de ID: ${playlist_id} no pertenece al usuario` })
+            return
+        }
+
         next()
     } catch (error) {
         console.error(error)
